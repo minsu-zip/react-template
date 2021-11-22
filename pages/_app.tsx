17ebcf76c99cb8ff47bc4e2ad12a61a461a6365f
Muntari29 <muntrock@gmail.com>
2021-11-22 17:16:53 +0900

    build: Eslint + Prettier + Ts 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
+  const a = 123
+  const b = [1, 2, 3, 4]
+  b.forEach((el) => console.log(el))
+
   return <Component {...pageProps} />
 }
 
